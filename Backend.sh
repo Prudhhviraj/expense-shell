348289dcfb0dff3bf40faf7e63873bf0088cfd86
prudh <prudhhviraj2@gmail.com>
2025-03-23 22:21:48 +0530

    just checking

--- Backend.sh
+++ Backend.sh
@@ -2,10 +2,10 @@
 
 USERID=$(id -u) #create a variable to get the USER ID by -u from user details
 #below are the color codes
-R="/e[31m" #RED
-G="/e[32m" #GREEN
-Y="/e[33m" #YELLOW
-N="/e[0m" #NORMAL
+R="\e[31m" #RED
+G="\e[32m" #GREEN
+Y="\e[33m" #YELLOW
+N="\e[0m" #NORMAL
 
 LOGS_FOLDER="/var/log/expense-logs" #create a log folder
 LOG_FILE=$(echo $0 | cut -d "." -f1) #LOG file Name $(echo $0 --> get the files ex:backend.sh to cut the .sh will use pipe with cut -d "." -f1 is fragment 1)
@@ -34,3 +34,54 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Disabling existing default NodeJS"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling NodeJS 20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing NodeJS"
+
+id expense &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOG_FILE_NAME
+    VALIDATE $? "Adding expense user"
+else
+    echo -e "expense user already exists ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOG_FILE_NAME
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading backend"
+
+cd /app
+rm -rf /app/*
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unzip backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+# Prepare MySQL Schema
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.daws82s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up the transactions schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Daemon Reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend"
+
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Backend"
