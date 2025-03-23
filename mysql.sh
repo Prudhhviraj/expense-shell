b1f389fd0f241cc22ee90c1b79b6dda2c2a4b0f2
prudh <prudhhviraj2@gmail.com>
2025-03-23 22:08:36 +0530

    Color code has been fixed from '/' to '\'

--- mysql.sh
+++ mysql.sh
@@ -2,10 +2,10 @@
 
 USERID=$(id -u) #create a variable to get the USER ID by -u from user details
 #below are the color codes
-R="/e[31m" #RED
-G="/e[32m" #GREEN
-Y="/e[33m" #YELLOW
-N="/e[0m" #NORMAL
+R="\e[31m" #RED
+G="\e[32m" #GREEN
+Y="\e[33m" #YELLOW
+N="\e[0m" #NORMAL
 
 LOGS_FOLDER="/var/log/expense-logs" #create a log folder
 LOG_FILE=$(echo $0 | cut -d "." -f1) #LOG file Name $(echo $0 --> get the files ex:backend.sh to cut the .sh will use pipe with cut -d "." -f1 is fragment 1)
